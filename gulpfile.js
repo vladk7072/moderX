2c97c107d27108ce0ad36efe9aa5938963e049f6
vladk7072 <vladk7072@gmail.com>
2022-01-19 18:05:53 +0200

    19.01.2022

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/swiper/swiper-bundle.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
